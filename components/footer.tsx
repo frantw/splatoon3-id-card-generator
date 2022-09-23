36c6b528a1cc2242a7ae2f0dafbb49a9d24b73ec
Fran <wei840220@gmail.com>
2022-09-23 22:00:57 +0800

    feat: adjust footer text

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,5 @@
 import { Box, Container, Stack, Text, useColorModeValue, Divider } from '@chakra-ui/react';
+import { Link } from '@chakra-ui/react';
 
 const Footer = () => {
     return (
@@ -10,15 +11,23 @@ const Footer = () => {
             <Divider />
             <Container
                 as={Stack}
+                w={'full'}
                 maxW={'5xl'}
+                px={0}
                 py={4}
                 direction={{ base: 'column', md: 'row' }}
                 spacing={4}
                 justify={{ base: 'center', md: 'space-between' }}
                 align={{ base: 'center', md: 'center' }}
             >
-                <Text>© 2022 Chakra Templates. All rights reserved</Text>
-                <Stack direction={'row'} spacing={6}></Stack>
+                <Text color={'gray.500'}>Splatoon 3 名片產生器 © 2022</Text>
+                <Text color={'gray.500'}>
+                    本網站所使用之名片模板由{' '}
+                    <Link color={'#1DA1F2'} href='https://twitter.com/roo_kie_art'>
+                        Roo（@roo_kie_art）
+                    </Link>{' '}
+                    提供
+                </Text>
             </Container>
         </Box>
     );
