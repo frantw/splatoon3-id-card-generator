8c0b9e0f4bf4fd6abdfb2189ae8b0bdd4ec9a447
Fran <wei840220@gmail.com>
2022-09-23 23:43:22 +0800

    feat: add og info

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
                 <title>Splatoon 3 名片產生器</title>
                 <meta
                     name='description'
-                    content='Nintendo Switch 遊戲《 斯普拉遁3（Splatoon 3）》之 遊戲名片 & 打工仔員工證 線上產生器'
+                    content='Nintendo Switch 遊戲《 斯普拉遁 3（Splatoon 3）》之 遊戲名片 & 打工仔員工證 線上產生器'
                 />
                 <meta name='viewport' content='width=device-width, initial-scale=1.0' />
                 <meta name='keywords' content='Splatoon 3, 斯普拉遁3, 遊戲名片, 產生器, Nintendo Switch, NS' />
@@ -32,6 +32,15 @@ const Home: NextPage = () => {
                     crossOrigin=''
                 />
                 <link rel='icon' href='/favicon.ico' />
+                <meta property='og:type' content='website' />
+                <meta property='og:title' content='Splatoon 3 名片產生器' />
+                <meta
+                    property='og:description'
+                    content='Nintendo Switch 遊戲《 斯普拉遁 3（Splatoon 3）》之 遊戲名片 & 打工仔員工證 線上產生器'
+                />
+                <meta property='og:image' content='/img/og-image.png' />
+                <meta property='og:url' content='https://splatoon3-card.vercel.app' />
+                <meta property='og:site_name' content='Splatoon 3 名片產生器' />
             </Head>
             <Container maxW={'full'} p={0}>
                 <Header />
