916113c338eede12294272e8616c7c4deb7824c8
Fran <wei840220@gmail.com>
2022-09-24 14:24:45 +0800

    feat: delay fonts preload

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,37 +17,10 @@ const Home: NextPage = () => {
                 />
                 <meta name='viewport' content='width=device-width, initial-scale=1.0' />
                 <meta name='keywords' content='Splatoon 3, 斯普拉遁3, 遊戲名片, 產生器, Nintendo Switch, NS' />
-                <link
-                    rel='preload'
-                    href='https://cdn.jsdelivr.net/gh/max32002/naikaifont@1.0/webfont/NaikaiFont-Regular-Lite.woff2'
-                    as='font'
-                    type='font/woff2'
-                    crossOrigin='anonymous'
-                />
-                <link
-                    rel='preload'
-                    href='https://cdn.jsdelivr.net/gh/max32002/FakePearl@1.1/webfont/FakePearl-Regular.woff2'
-                    as='font'
-                    type='font/woff2'
-                    crossOrigin='anonymous'
-                />
+                <link rel='preconnect' href='https://cdn.jsdelivr.net'></link>
                 <link
                     rel='preload'
                     href='https://cdn.jsdelivr.net/gh/max32002/TaiwanPearl@2.125/webfont/TaiwanPearl-Regular.woff2'
-                    as='font'
-                    type='font/woff2'
-                    crossOrigin='anonymous'
-                />
-                <link
-                    rel='preload'
-                    href='https://cdn.jsdelivr.net/gh/max32002/nanifont@1.036/webfont/NaniFont-Regular.woff2'
-                    as='font'
-                    type='font/woff2'
-                    crossOrigin='anonymous'
-                />
-                <link
-                    rel='preload'
-                    href='https://cdn.jsdelivr.net/gh/max32002/JasonHandWritingFonts@20220828/webfont/JasonHandwriting1-SemiBold.woff2'
                     as='font'
                     type='font/woff2'
                     crossOrigin='anonymous'
@@ -64,6 +37,35 @@ const Home: NextPage = () => {
                 <meta property='og:url' content='https://splatoon-3.vercel.app' />
                 <meta property='og:site_name' content='Splatoon 3 名片產生器' />
             </Head>
+            <link
+                rel='preload'
+                href='https://cdn.jsdelivr.net/gh/max32002/naikaifont@1.0/webfont/NaikaiFont-Regular-Lite.woff2'
+                as='font'
+                type='font/woff2'
+                crossOrigin='anonymous'
+            />
+            <link
+                rel='preload'
+                href='https://cdn.jsdelivr.net/gh/max32002/FakePearl@1.1/webfont/FakePearl-Regular.woff2'
+                as='font'
+                type='font/woff2'
+                crossOrigin='anonymous'
+            />
+
+            <link
+                rel='preload'
+                href='https://cdn.jsdelivr.net/gh/max32002/nanifont@1.036/webfont/NaniFont-Regular.woff2'
+                as='font'
+                type='font/woff2'
+                crossOrigin='anonymous'
+            />
+            <link
+                rel='preload'
+                href='https://cdn.jsdelivr.net/gh/max32002/JasonHandWritingFonts@20220828/webfont/JasonHandwriting1-SemiBold.woff2'
+                as='font'
+                type='font/woff2'
+                crossOrigin='anonymous'
+            />
             <Container maxW={'full'} p={0}>
                 <Header />
                 <Center />
